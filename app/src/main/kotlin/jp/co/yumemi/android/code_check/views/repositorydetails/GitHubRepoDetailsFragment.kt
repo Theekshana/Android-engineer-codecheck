301c70672053488b8dfc51ce9cfdfc2fbc80b6f1
Theekshana <tvimukthi.m@gmail.com>
2024-04-26 18:14:25 +0900

    Add styles.xml for app-wide UI customization

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/views/repositorydetails/GitHubRepoDetailsFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/views/repositorydetails/GitHubRepoDetailsFragment.kt
@@ -3,15 +3,8 @@
  */
 package jp.co.yumemi.android.code_check.views.repositorydetails
 
-import android.os.Bundle
-import android.util.Log
-import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.navArgs
-import coil.load
 import jp.co.yumemi.android.code_check.R
-import jp.co.yumemi.android.code_check.activities.MainActivity.Companion.lastSearchDate
-import jp.co.yumemi.android.code_check.databinding.FragmentRepoDetailsBinding
 
 
 class GitHubRepoDetailsFragment : Fragment(R.layout.fragment_repo_details) {
