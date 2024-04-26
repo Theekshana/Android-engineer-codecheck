580d2aaa5345b3d8b6cda57708da7e2fb5d8489d
Theekshana <tvimukthi.m@gmail.com>
2024-04-26 11:59:18 +0900

    Implement getGithubAccountsFromDataSource in GithubRepository

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/repository/GithubRepository.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/repository/GithubRepository.kt
@@ -1,4 +1,22 @@
 package jp.co.yumemi.android.code_check.repository
 
+import jp.co.yumemi.android.code_check.model.DataStatus
+import jp.co.yumemi.android.code_check.model.GitHubAccount
+import kotlinx.coroutines.flow.Flow
+
+/**
+ * Interface for retrieving GitHub accounts from a data source.
+ */
 interface GithubRepository {
+    /**
+     * Retrieves GitHub accounts based on the specified search query.
+     *
+     * @param searchQuery The search query used to filter GitHub accounts.
+     * @return A Flow emitting DataStatus objects containing the list of GitHub accounts.
+     */
+    suspend fun getGithubAccountsFromDataSource(
+        // The search query used to filter GitHub accounts.
+        searchQuery: String
+    ): Flow<DataStatus<List<GitHubAccount>>>
+
 }
