cc1af60767f5717e2bae9996b5a8f422e3c53400
Theekshana <tvimukthi.m@gmail.com>
2024-04-27 23:01:17 +0900

    Add navigation to GitHub repository details screen

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/views/repositorydetails/GitHubRepoDetailsFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/views/repositorydetails/GitHubRepoDetailsFragment.kt
@@ -7,28 +7,7 @@ import androidx.fragment.app.Fragment
 import jp.co.yumemi.android.code_check.R
 
 
-class GitHubRepoDetailsFragment : Fragment(R.layout.fragment_repo_details) {
+class GitHubRepoDetailsFragment : Fragment() {
 
-    /*private val args: GitHubRepoDetailsFragmentArgs by navArgs()
 
-    private var binding: FragmentRepoDetailsBinding? = null
-    private val _binding get() = binding!!
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        Log.d("検索した日時", lastSearchDate.toString())
-
-        binding = FragmentRepoDetailsBinding.bind(view)
-
-        var item = args.item
-
-        _binding.ownerIconView.load(item.ownerIconUrl);
-        _binding.nameView.text = item.name;
-        _binding.languageView.text = item.language;
-        _binding.starsView.text = "${item.stargazersCount} stars";
-        _binding.watchersView.text = "${item.watchersCount} watchers";
-        _binding.forksView.text = "${item.forksCount} forks";
-        _binding.openIssuesView.text = "${item.openIssuesCount} open issues";
-    }*/
 }
