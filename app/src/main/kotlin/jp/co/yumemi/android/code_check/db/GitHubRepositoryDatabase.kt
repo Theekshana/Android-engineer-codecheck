af3e1ce858bba1ba331bc031ad8c17aa3fc8c0e6
Theekshana <tvimukthi.m@gmail.com>
2024-04-29 12:25:22 +0900

    Fix error in GitHubRepositoryDatabase class

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/db/GitHubRepositoryDatabase.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/db/GitHubRepositoryDatabase.kt
@@ -3,13 +3,14 @@ package jp.co.yumemi.android.code_check.db
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import jp.co.yumemi.android.code_check.model.GitHubAccount
 
 /**
  * Abstract class representing the Room database for GitHub repositories.
  * Extend this class to create a Room database instance.
  */
 
-@Database(entities = [GitHubRepositoryDatabase::class], version = 1, exportSchema = false)
+@Database(entities = [GitHubAccount::class], version = 1, exportSchema = false)
 @TypeConverters(OwnerConverter::class)
 abstract class GitHubRepositoryDatabase : RoomDatabase() {
 
