b28da07e421535ad72fde1a25663dfbf3f56e4f4
Theekshana <tvimukthi.m@gmail.com>
2024-04-30 17:47:17 +0900

    Implement setupWebView function for load web page

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/views/favourite/WebViewFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/views/favourite/WebViewFragment.kt
@@ -1,23 +1,71 @@
 package jp.co.yumemi.android.code_check.views.favourite
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import jp.co.yumemi.android.code_check.R
+import android.webkit.WebViewClient
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.navArgs
+import dagger.hilt.android.AndroidEntryPoint
+import jp.co.yumemi.android.code_check.databinding.FragmentWebViewBinding
 
-
+/**
+ * A Fragment that displays a web view.
+ * This Fragment is responsible for displaying a web page.
+ */
+@AndroidEntryPoint
 class WebViewFragment : Fragment() {
 
+    // Data binding for the fragment layout
+    private lateinit var binding: FragmentWebViewBinding
+
+    // Arguments passed to the fragment using Safe Args
+    private val args: WebViewFragmentArgs by navArgs()
+
+    // ViewModel for managing favorites data
+    private lateinit var viewModel: FavoritesViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_web_view, container, false)
+    ): View {
+        binding = FragmentWebViewBinding.inflate(
+            inflater,
+            container,
+            false
+        )
+        // Initialize ViewModel using ViewModelProvider
+        viewModel = ViewModelProvider(this)[FavoritesViewModel::class.java]
+
+        return binding.root
     }
 
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(view, savedInstanceState)
 
+        // Retrieve the target repository from the arguments
+        val webRepository = args.targetRepository
+
+        // Set up the WebView with the HTML URL of the repository owner
+        setupWebView(webRepository.owner?.htmlUrl)
+
+    }
+
+    /**
+     * Sets up the WebView with the specified HTML URL.
+     * @param htmlUrl The HTML URL to load in the WebView.
+     */
+    private fun setupWebView(htmlUrl: String?) {
+        binding.webView.apply {
+            webViewClient = WebViewClient()
+            htmlUrl?.let {
+                loadUrl(it)
+            }
+        }
+    }
 }
