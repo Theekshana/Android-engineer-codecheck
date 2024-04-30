ca824e48297f4702e53134a8ce16cfd72d8cb610
Theekshana <tvimukthi.m@gmail.com>
2024-04-30 13:04:30 +0900

    Added BindingAdapter to display short names extracted from full names in TextViews

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/util/BindingAdapters.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/util/BindingAdapters.kt
@@ -1,6 +1,7 @@
 package jp.co.yumemi.android.code_check.util
 
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import jp.co.yumemi.android.code_check.R
@@ -18,5 +19,23 @@ fun loadImage(view: ImageView, url: String?) {
             .load(imageUrl)
             .placeholder(R.drawable.ic_image_placeholder)
             .into(view)
+    }
+}
+
+/**
+ * Binding adapter to set the short name for a TextView from a full name string.
+ *
+ * @param textView The TextView to set the short name.
+ * @param fullName The full name string from which to extract the short name.
+ */
+@BindingAdapter("shortName")
+fun setShortName(textView: TextView, fullName: String?) {
+    fullName?.let {
+        val parts = fullName.split("/")
+        if (parts.size == 2) {
+            textView.text = parts[1]
+        } else {
+            textView.text = fullName
+        }
     }
 }
