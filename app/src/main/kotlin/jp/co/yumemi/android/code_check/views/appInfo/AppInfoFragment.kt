4e1d539944afefeaa3266fef03dc8aef5505172c
Theekshana <tvimukthi.m@gmail.com>
2024-05-01 16:59:01 +0900

    Add data binding in AppInfoFragment

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/views/appInfo/AppInfoFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/views/appInfo/AppInfoFragment.kt
@@ -5,19 +5,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import jp.co.yumemi.android.code_check.R
+import jp.co.yumemi.android.code_check.databinding.FragmentAppInfoBinding
 
-
+/**
+ * A fragment to display information about the app.
+ * This fragment includes details.
+ */
 class AppInfoFragment : Fragment() {
 
+    // Data binding for the fragment layout
+    private lateinit var binding: FragmentAppInfoBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_app_info, container, false)
+    ): View {
+        binding = FragmentAppInfoBinding.inflate(
+            inflater,
+            container,
+            false
+        )
+        return binding.root
     }
-
-
 }
